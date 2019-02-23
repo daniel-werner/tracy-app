4c43557e51d93e2e174f72e107de72fd87766c44
Daniel Werner <vernerd@gmail.com>
2019-02-23 21:44:15 +0100

    created workout related classes

---
+++ www/js/workout/app.workout.cycling_workout.js
@@ -0,0 +1,14 @@
+;(function (root) {
+
+    var CyclingWorkout = function () {
+        BaseWorkout.call(this);
+
+        this.type = BaseWorkout.WORKOUT_TYPE_CYCLING;
+    };
+
+    var proto = new BaseWorkout();
+
+    CyclingWorkout.prototype = proto;
+
+    root.CyclingWorkout = CyclingWorkout;
+})(window);
