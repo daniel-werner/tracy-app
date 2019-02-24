16d73d37b5f34c41c3032b87ce06720dfba4d7d3
Daniel Werner <vernerd@gmail.com>
2019-02-24 13:01:12 +0100

    Unit test for hardware driver factory

--- www/tests/spec/unit/DriverFactorySpec.js
+++ www/tests/spec/unit/DriverFactorySpec.js
@@ -4,10 +4,12 @@ describe("DriverFactory", function () {
         var platform = Platform.PLATFORM_TIZEN,
             driverFactory = new DriverFactory(platform),
             batteryDriver = driverFactory.buildBatteryDriver(platform),
-            networkDriver = driverFactory.buildNetworkDriver(platform);
+            networkDriver = driverFactory.buildNetworkDriver(platform),
+            hardwareDriver = driverFactory.buildHardwareDriver(platform);
 
             expect( batteryDriver instanceof BatteryDriverTizen ).toBeTruthy();
             expect( networkDriver instanceof NetworkDriverTizen ).toBeTruthy();
+            expect( hardwareDriver instanceof HardwareDriverTizen ).toBeTruthy();
     });
 
     it('should create drivers for android', function () {
@@ -15,9 +17,11 @@ describe("DriverFactory", function () {
         var platform = Platform.PLATFORM_ANDROID,
             driverFactory = new DriverFactory(platform),
             batteryDriver = driverFactory.buildBatteryDriver(platform),
-            networkDriver = driverFactory.buildNetworkDriver(platform);
+            networkDriver = driverFactory.buildNetworkDriver(platform),
+            hardwareDriver = driverFactory.buildHardwareDriver(platform);
 
             expect( batteryDriver instanceof BatteryDriverAndroid ).toBeTruthy();
             expect( networkDriver instanceof NetworkDriverAndroid ).toBeTruthy();
+            expect( hardwareDriver instanceof HardwareDriverAndroid ).toBeTruthy();
     });
 });
