543c92769081960970415026eee1aefb597d3903
Daniel Werner <vernerd@gmail.com>
2021-11-27 19:37:16 +0100

    Clean up for new flutter project

--- src/js/model/models.js
+++
@@ -1,7 +0,0 @@
-require('../common/app.common.events');
-
-require('./app.model.battery');
-require('./app.model.network');
-require('./app.model.geolocation');
-require('./app.model.workout');
-require('./app.model.sync');
--- www/README.md
+++
@@ -1,3 +0,0 @@
-# Tracy app
-Fitness tracking mobile app. Record running and cycling on a GSP enabled mobile
-device and upload the workout data to the tracy web app for further analysis.
