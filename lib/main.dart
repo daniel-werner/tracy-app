f49e7528c35c0e33af918a3a7937ce1dc2b2813d
Daniel Werner <vernerd@gmail.com>
2022-12-24 16:52:27 +0100

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,42 @@ import 'package:provider/provider.dart';
 import 'home.dart';
 import 'models/connections.dart';
 import 'workout.dart';
+import 'package:catcher/catcher.dart';
 
-void main() => runApp(Tracy());
+void main() {
+  /// STEP 1. Create catcher configuration.
+  /// Debug configuration with dialog report mode and console handler. It will show dialog and once user accepts it, error will be shown   /// in console.
+  CatcherOptions debugOptions = CatcherOptions(SilentReportMode(), [
+    SnackbarHandler(
+      const Duration(seconds: 20),
+      backgroundColor: Colors.red,
+      elevation: 2,
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      behavior: SnackBarBehavior.floating,
+      action: SnackBarAction(
+          label: "OK", textColor: Colors.white, onPressed: () {}),
+      textStyle: const TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+      ),
+    ),
+  ]);
+
+  /// Release configuration. Same as above, but once user accepts dialog, user will be prompted to send email with crash to support.
+  CatcherOptions releaseOptions = CatcherOptions(DialogReportMode(), [
+    ConsoleHandler()
+    // EmailManualHandler(["daniel@wdev.rs"])
+  ]);
+
+  /// STEP 2. Pass your root widget (MyApp) along with Catcher configuration:
+  Catcher(
+      rootWidget: Tracy(),
+      debugConfig: debugOptions,
+      releaseConfig: releaseOptions);
+// runApp(Tracy());
+}
 
 const Color primaryGreenColor = Color(0xFFAFFD58);
 
@@ -51,6 +85,9 @@ class Tracy extends StatelessWidget {
                       TextStyle(fontSize: 14.0, color: primaryGreenColor),
                 ),
               ),
+
+              /// STEP 3. Add navigator key from Catcher. It will be used to navigate user to report page or to show dialog.
+              navigatorKey: Catcher.navigatorKey,
               initialRoute: '/',
               routes: {
                 '/': (context) => const Home(),
